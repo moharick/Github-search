51af78f7d4c69afb98c2855e053b9118be2f3df4
moharick <moharick@gmail.com>
2019-08-26 11:04:05 +0300

    add user class.

---
+++ src/app/user.ts
@@ -0,0 +1,15 @@
+export class User {
+  // tslint:disable-next-line:variable-name
+  created_at: any;
+  // tslint:disable-next-line:variable-name
+  avatar_url: any;
+  // tslint:disable-next-line:variable-name
+  followers_url: any;
+  // tslint:disable-next-line:variable-name
+  following_url: any;
+  // tslint:disable-next-line:variable-name
+  repos_url: any;
+  login: any;
+  // tslint:disable-next-line:variable-name
+  public_repos: any;
+}
